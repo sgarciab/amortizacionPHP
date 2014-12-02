2dd337368fd123c31ef90061d69fec5feb2c550a
unknown <sgarcia@PPM-DV03.ppm.com.ec>
2014-12-02 16:44:19 -0500

    Version Final

--- tablasAmortizacion.php
+++ tablasAmortizacion.php
@@ -5,17 +5,47 @@ $capital = $_POST['capital'];
 $tipo =$_POST['tipo'];
 $plazo = $_POST['plazo'];
 $interes=0.1123;
-            
+
+$optionsAmortizacion=array('12'=>'Mensual','4'=>'Trimestal','2'=>'Semestral');
+
+$tipoAmortizacion=$optionsAmortizacion[$tipo];
+
 $prestamo= new Prestamo();    
 $prestamo->setCapital($capital);
 $prestamo->setIterador(intval($tipo));
 $prestamo->setTasaInteres($interes);
-
 $prestamo->pagos(1, $plazo, $prestamo->calcCuotaFrancesa($plazo));
 
-imprimirTablas($prestamo->calcTablaDePagos(false), $prestamo->calcTablaDePagos(true));
 
-function imprimirTablas($tablaFrancesa, $tablaAlemana,$dec=2)
+$amortFrancesa=$prestamo->calcTablaDePagos(false);
+$amortAlemana=$prestamo->calcTablaDePagos(true);
+
+$datosFrancesa=array();
+$datosFrancesa['cuota']=0;
+$datosFrancesa['interes']=0;
+$datosFrancesa['tasa']=$interes*100;
+foreach ($amortFrancesa as $value)
+{
+     $datosFrancesa['interes']=$datosFrancesa['interes']+$value['Interes'];
+     $datosFrancesa['cuota']=$value['Cuota'];
+}
+
+
+$datosAlemana=array();
+$datosAlemana['cuota']=0;
+$datosAlemana['interes']=0;
+$datosAlemana['tasa']=$interes*100;
+foreach ($amortAlemana as $value)
+{
+     $datosAlemana['interes']=$datosAlemana['interes']+$value['Interes'];
+     $datosAlemana['cuota']=$value['Amortizacion'];
+}
+
+    
+imprimirTablas($amortFrancesa, $amortAlemana,$datosFrancesa,$datosAlemana,$tipoAmortizacion);
+
+
+function imprimirTablas($tablaFrancesa, $tablaAlemana,$datosFrancesa,$datosAlemana,$tipoAmortizacion,$dec=2)
 {
    require 'tablasAmortizacion.template.php';
 }
--- tablasAmortizacion.template.php
+++ tablasAmortizacion.template.php
@@ -2,7 +2,15 @@
 <div id="contentTables">
     
     <div id="divFrancesa">
-        <h4>Amortizacion Francesa <small> Cuotas Iguales</small></h4>
+        <h3>Amortizacion Francesa <small> Cuotas Iguales</small></h3>
+        <hr>
+        <table  class="table table-bordered">
+            <tr><td><strong>Cuota <?= $tipoAmortizacion ?></strong></td><td>$<?= number_format($datosFrancesa['cuota'], $dec, ',', '.')   ?></td></tr>
+            <tr><td><strong>Inter&eacute;s Acumulado</strong></td><td>$<?= number_format($datosFrancesa['interes'], $dec, ',', '.')   ?></td></tr>
+            <tr><td><strong>Tasa de Inter&eacute;s Nominal</strong></td><td><?= number_format($datosFrancesa['tasa'], $dec, ',', '.')   ?>%</td></tr>
+        </table>
+        
+        <hr>
         <table  class="table table-bordered">
             <th>Periodo</th><th>Cuota</th><th>Capital</th><th>Inter&eacute;s</th><th>Saldo Capital</th>
             <?php    foreach ($tablaFrancesa as $value): ?>
@@ -29,9 +37,18 @@
         </table>  
     </div>
     
-    
+    <hr>
+    <hr>
     <div id="divAlemana" class="divAmort">
-        <h4>Amortizacion Alemana <small>Iguales Aportes al Capital </small></h4>
+        <h3>Amortizacion Alemana <small>Iguales Aportes al Capital </small></h3>
+        <hr>
+        <table  class="table table-bordered">
+            <tr><td><strong>Cuota de Capital <?= $tipoAmortizacion ?></strong></td><td>$<?= number_format($datosAlemana['cuota'], $dec, ',', '.')   ?></td></tr>
+            <tr><td><strong>Inter&eacute;s Acumulado</strong></td><td>$<?= number_format($datosAlemana['interes'], $dec, ',', '.')   ?></td></tr>
+            <tr><td><strong>Tasa de Inter&eacute;s Nominal</strong></td><td><?= number_format($datosAlemana['tasa'], $dec, ',', '.')   ?>%</td></tr>
+        </table>
+        
+        <hr>
         <table class="table table-bordered">
           <th>Periodo</th><th>Cuota</th><th>Capital</th><th>Inter&eacute;s</th><th>Saldo Capital</th>
             <?php    foreach ($tablaAlemana as $value): ?>
